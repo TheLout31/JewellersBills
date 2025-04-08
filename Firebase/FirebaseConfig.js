372fa61d4647fb11c0b2137d761724bee5061c77
imranjaleel <imranjaleel2331@gmail.com>
2025-04-08 11:20:11 +0530

    commit!!

--- Firebase/FirebaseConfig.js
+++ Firebase/FirebaseConfig.js
@@ -1,12 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC5WmeIcaDjI0kdxGi9EY7FqenD8EGbAdE",
+ apiKey:"AIzaSyC5WmeIcaDjI0kdxGi9EY7FqenD8EGbAdE",
   authDomain: "jewellersbills.firebaseapp.com",
   projectId: "jewellersbills",
   storageBucket: "jewellersbills.firebasestorage.app",
